5540a162835b3a4173c1f74a1d42fc7c1c4a9931
Destcable <artmobpavlov21@gmail.com>
2024-06-08 17:56:23 +0300

    [upd]

--- src/ui/Sidebar/index.jsx
+++ src/ui/Sidebar/index.jsx
@@ -6,18 +6,21 @@ const Sidebar = () => {
         borderRight: '1px solid #f0f0f0'
     };
 
-    const sidebarItemStyle = { 
-        borderBottom: '1px solid #f0f0f0'
-    };
-
     return(
         <div className=" h-100" style={sidebarStyle}>
-            <div className="d-flex align-items-center p-3" style={sidebarItemStyle}>
-                <LiaHomeSolid className="me-2" style={{ fontSize: "30px"}} /> Home
-            </div>
+            <SidebarItem
+                icon={<LiaHomeSolid className="me-2" style={{ fontSize: "30px"}} />}
+                name='Home'
+            />
             <div className="p-3">Manage Returns</div>
         </div>
     )
 };
+
+const SidebarItem = ({ icon, name }) => (
+    <div className="d-flex align-items-center p-3 sidebar__item--border-bottom">
+        {icon} {name}
+    </div>
+);
 
 export default Sidebar;
