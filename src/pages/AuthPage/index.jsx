06de5afb8429ccae8272b2fb75d65e66c5509957
Destcable <artmobpavlov21@gmail.com>
2024-06-09 17:08:55 +0300

    [upd]

--- src/pages/AuthPage/index.jsx
+++ src/pages/AuthPage/index.jsx
@@ -1,17 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import SapLogo from "../../lib/icons/SapLogo.png";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const onSubmit = () => {
+        navigate('/');
+    };
+
     return (
         <div className="bg-light" style={styles.container}>
             <div style={styles.row}>
-                <div className="bg-white" style={styles.col}>
-                    <div style={styles.card}>
+                <div style={styles.col}>
+                    <div className="bg-white" style={styles.card}>
                         <div style={styles.cardBody}>
                             <div style={styles.logoContainer}>
                                 <img src={SapLogo} alt="SAP Logo" style={styles.logo} />
                             </div>
-                            <form>
-                                <div className="form-group">
+                            <form onSubmit={onSubmit}>
+                                <div className="form-group"style={{ marginTop: '40px'}}>
                                     <label className="text-secondary" htmlFor="username">User Name:</label>
                                     <input
                                         type="text"
@@ -21,32 +28,31 @@ const Login = () => {
                                         style={styles.borderInput}
                                     />
                                 </div>
-                                <div className="form-group">
+                                <div className="form-group" style={{ marginTop: '20px'}}>
                                     <label className="text-secondary" htmlFor="password">Password:</label>
                                     <input
                                         type="password"
                                         className="form-control rounded-1"
-                                        id="password"
                                         placeholder="Password"
                                         style={styles.borderInput}
                                     />
                                 </div>
-                                <div className="form-check mb-2">
+                                <div className="form-check" style={{ marginTop: '20px'}}>
                                     <input
                                         type="checkbox"
-                                        className="form-check-input"
+                                        className="form-check-input rounded-0"
                                         id="keepSignedIn"
                                         style={styles.borderInput}
                                     />
-                                    <label className="form-check-label" htmlFor="keepSignedIn">
+                                    <label className="form-check-label text-secondary" htmlFor="keepSignedIn">
                                         Keep me signed in
                                     </label>
                                 </div>
-                                <div className="text-center">
-                                    <a href="#" className="d-block mb-3">
+                                <div>
+                                    <a style={{ marginTop: '20px', color: '#0A6ED1'}} href="#" className="d-block text-decoration-none">
                                         Forgot Password?
                                     </a>
-                                    <button type="submit" className="btn btn-primary">
+                                    <button type="submit" className="btn w-100 text-white rounded-1" style={{ marginTop: '35px', backgroundColor: '#0A6ED1'}}>
                                         Sign In
                                     </button>
                                 </div>
@@ -72,15 +78,19 @@ const styles = {
         width: '100%',
     },
     col: {
-        maxWidth: '400px',
+        maxWidth: '500px',
         width: '100%',
     },
     card: {
         border: 'none',
         boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        paddingTop: '60px',
+        paddingLeft: '100px',
+        paddingRight: '100px',
+        paddingBottom: '45px'
     },
     cardBody: {
-        padding: '30px',
+        // padding: '20px',
     },
     logoContainer: {
         textAlign: 'center',
